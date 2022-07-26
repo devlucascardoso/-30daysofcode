00d0ef6fd4276ae77053dd03d4681997a445e35e
Lucas Cardoso <lucascardoso946@gmail.com>
2022-07-25 22:34:45 -0300

    javascript fundamentals - Day 01

---
+++ 01-variables_and_scope.js
@@ -0,0 +1,22 @@
+//RESUMO
+//Podemos declarar variáveis para armazenar dados usando as palavras-chave abaixo:
+//let – é uma declaração variável moderna.
+//const – é como, mas o valor da variável não pode ser alterado.
+//var – Não usar.
+
+//1- LET || É possivel alterar dados da variável.
+let firstName
+firstName = 'Lucas'
+console.log(firstName)
+firstName = 'Lucas Cardoso'
+console.log(firstName)
+//Console:
+//Lucas
+//Lucas Cardoso
+
+//2- CONST
+const myBirthday = '18.04.1982'
+console.log(myBirthday)
+
+//3- var
+//É uma constante porém pode alterar os dados :/
